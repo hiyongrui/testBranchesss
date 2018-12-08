07fef7fc554838b34ddd73bf5fb80940d1dd6f77
hiyongrui <172808h@mymail.nyp.edu.sg>
2018-12-08 12:28:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tanyongrui/testbranch/testttBraaanch.kt
@@ -0,0 +1,12 @@
+package com.example.tanyongrui.testbranch
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class testttBraaanch : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_testtt_braaanch)
+    }
+}
